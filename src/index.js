003e44a7c9d139fcfbf4a75a26f9c062900d7f1e
Vineetsh7 <119120435+Vineet1sh@users.noreply.github.com>
2024-09-08 19:57:51 +0530

    "hi"

--- src/index.js
+++ src/index.js
@@ -14,26 +14,3 @@ connectDB()
     .catch((error) => {
         console.log("MongoDB connection failed !!!", error)
     })
-
-
-/*
-import express from "express"
-const app = express()
-
-    (async () => {
-        try {
-            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-            app.on("error", () => {
-                console.log("ERROR: ", error)
-                throw error
-            })
-            app.listen(process.env.PORT, () => {
-                console.log(`App is listening on ${process.env.PORT}`);
-            });
-        } catch (error) {
-            console.error("ERROR: ", error)
-            throw error
-
-        }
-    })()
-*/
